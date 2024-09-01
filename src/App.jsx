d0404099338e6e906f7e46fdf7ffc6a3a8f40981
Luis Arzola <luisarzola3@gmail.com>
2024-09-01 00:30:19 -0700

    Design adjustments and final details

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,61 @@ import PokemonCard from "./components/PokemonCard";
 import "./app.scss";
 
 function App() {
-	const [pokemonNames, setPokemonNames] = useState([]);
-	const [pokemonInfo, setPokemonInfo] = useState({});
-	const [isFetching, setIsFetching] = useState(false);
+    const [pokemonNames, setPokemonNames] = useState([]);
+    const [pokemonInfo, setPokemonInfo] = useState({});
+    const [isFetching, setIsFetching] = useState(false);
+    const [activePokemon, setActivePokemon] = useState("");
 
-	const apiURL = "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0";
+    const apiURL = "https://pokeapi.co/api/v2/pokemon?limit=250&offset=0";
 
-	const getPokemonList = async (url) => {
-		const response = await fetch(url);
-		const data = await response.json();
-		setPokemonNames(data.results);
-	};
+    const getPokemonList = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setPokemonNames(data.results);
+    };
 
-	const getPokemonInfo = async (pokeURL) => {
-		const response = await fetch(pokeURL);
-		const data = await response.json();
-		setPokemonInfo(data);
-		setIsFetching(true);
-	};
+    const getPokemonInfo = async (pokeURL) => {
+        const response = await fetch(pokeURL);
+        const data = await response.json();
+        setPokemonInfo(data);
+        setIsFetching(true);
+    };
 
-	const handleClick = (pokeURL) => {
-		getPokemonInfo(pokeURL);
-	};
+    const handleClick = (pokeURL, name) => {
+        getPokemonInfo(pokeURL);
+        setActivePokemon(name);
+    };
 
-	useEffect(() => {
-		getPokemonList(apiURL);
-		getPokemonInfo("https://pokeapi.co/api/v2/pokemon/1/");
-	}, []);
+    useEffect(() => {
+        getPokemonList(apiURL);
+        getPokemonInfo("https://pokeapi.co/api/v2/pokemon/1/");
+    }, []);
 
-	return (
-		<>
-			<h1 className="main_title">Pokemon App</h1>
+    return (
+        <>
+            <h1 className="main_title">Pokédex App</h1>
 
-			<div className="main-container">
-				<div className="list-container">
-					{pokemonNames.map((poke, index) => {
-						let formatIndex = ("000" + (index + 1)).slice(-3);
-						return (
-							<div key={index} onClick={() => handleClick(poke.url)}>
-								<p>
-									{formatIndex} {poke.name}
-								</p>
-							</div>
-						);
-					})}
-				</div>
-				<div className="card-container">{isFetching ? <PokemonCard pokemon={pokemonInfo} /> : ""}</div>
-			</div>
-		</>
-	);
+            <div className="main-container">
+                <div className="list-container">
+                    {pokemonNames.map((poke, index) => {
+                        let formatIndex = ("000" + (index + 1)).slice(-3);
+                        return (
+                            <div
+                                key={index}
+                                onClick={() => handleClick(poke.url, poke.name)}
+                                className={poke.name === activePokemon ? "active" : ""}
+                            >
+                                <p>
+                                    {formatIndex} {poke.name}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="card-container">{isFetching ? <PokemonCard pokemon={pokemonInfo} /> : ""}</div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,116 +3,135 @@ import "../styles/card.scss";
 import { useState, useEffect } from "react";
 
 const PokemonCard = ({ pokemon }) => {
-	const { sprites, types, species, id, abilities, height, weight, stats } = pokemon;
-	const [pokemonDescription, setPokemonDescription] = useState("");
+    const { sprites, types, species, id, abilities, height, weight, stats } = pokemon;
+    const [pokemonDescription, setPokemonDescription] = useState("");
+    const [isShiny, setIsShiny] = useState(false);
 
-	const weightKg = weight / 10;
-	const heightMeters = height / 10;
-	const formatId = ("000" + id).slice(-3);
+    const weightKg = weight / 10;
+    const heightMeters = height / 10;
+    const formatId = ("000" + id).slice(-3);
 
-	const getPokemonSpecies = async (pokeURL) => {
-		const response = await fetch(pokeURL);
-		const data = await response.json();
-		setPokemonDescription(data.flavor_text_entries[5].flavor_text);
-	};
+    const getPokemonSpecies = async (pokeURL) => {
+        const response = await fetch(pokeURL);
+        const data = await response.json();
+        setPokemonDescription(data.flavor_text_entries[5].flavor_text);
+    };
 
-	const description = pokemonDescription
-		.replace("\f", "\n")
-		.replace("\u00ad\n", "")
-		.replace("\u00ad", "")
-		.replace(" -\n", " - ")
-		.replace("-\n", "-")
-		.replace("\n", " ");
+    const description = pokemonDescription
+        .replace("\f", "\n")
+        .replace("\u00ad\n", "")
+        .replace("\u00ad", "")
+        .replace(" -\n", " - ")
+        .replace("-\n", "-")
+        .replace("\n", " ");
 
-	useEffect(() => {
-		getPokemonSpecies(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
-	}, [pokemon]);
+    useEffect(() => {
+        getPokemonSpecies(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    }, [pokemon]);
 
-	return (
-		<div className="card_container">
-			<div className="main_info">
-				<div className="number">No.{formatId}</div>
-				<div className="name">{species.name}</div>
-			</div>
-			<div className="sprite">
-				<img src={`./src/assets/sprites/${id}.gif`} alt="" />
-				{/* <img src={`./src/assets/shiny/${id}.gif`} alt="" /> */}
-			</div>
+    return (
+        <div className="card_container">
+            <div className="main_info">
+                <div className="number">No.{formatId}</div>
+                <div className="name">{species.name}</div>
+            </div>
+            <div className="sprite-container">
+                <div className="sprite">
+                    <img src={`/assets/${isShiny ? "shiny/" : "sprites/"}${id}.gif`} alt="" />
+                </div>
+                <div className="icon">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-sparkles"
+                        width="44"
+                        height="44"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="#ffffff"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        onClick={() => {
+                            setIsShiny(!isShiny);
+                        }}
+                        className={isShiny ? "shiny-active" : ""}
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" />
+                    </svg>
+                </div>
+            </div>
 
-			<div className="description">{description}</div>
-			<div className="flex_container">
-				<div className="type">
-					<div className="type_container">
-						{types.map((type, index) => {
-							return (
-								<span className={`type_${type.type.name}`} key={index}>
-									{type.type.name}
-								</span>
-							);
-						})}
-					</div>
-				</div>
-				<div className="measurements">
-					<div className="weight">
-						<span>Weight:</span> {weightKg}kg
-					</div>
-					<div className="height">
-						<span>Height:</span> {heightMeters}m
-					</div>
-				</div>
-			</div>
-			<div className="box_container">
-				<div className="stats_container">
-					{/* STATS */}
-					{stats.map((stat, key) => {
-						const { base_stat, stat: statInfo } = stat;
+            <div className="description">{description}</div>
+            <div className="flex_container">
+                <div className="type">
+                    <div className="type_container">
+                        {types.map((type, index) => {
+                            return (
+                                <span className={`type_${type.type.name}`} key={index}>
+                                    {type.type.name}
+                                </span>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="measurements">
+                    <div className="weight">
+                        <span>Weight:</span> {weightKg}kg
+                    </div>
+                    <div className="height">
+                        <span>Height:</span> {heightMeters}m
+                    </div>
+                </div>
+            </div>
+            <div className="box_container">
+                <div className="stats_container">
+                    {/* STATS */}
+                    {stats.map((stat, key) => {
+                        const { base_stat, stat: statInfo } = stat;
 
-						const fullStat = 255;
-						const statPercentage = (base_stat * 100) / fullStat;
+                        const fullStat = 255;
+                        const statPercentage = (base_stat * 100) / fullStat;
 
-						let color = "";
+                        const statNameMap = {
+                            "special-attack": "Sp. Atk",
+                            "special-defense": "Sp. Def",
+                        };
 
-						if (statInfo.name == "special-attack") {
-							statInfo.name = "Sp. Atk";
-						}
-						if (statInfo.name == "special-defense") {
-							statInfo.name = "Sp. Def";
-						}
+                        // Replace the original stat name if it exists in the map
+                        const statName = statNameMap[statInfo.name] || statInfo.name;
 
-						if ((base_stat > 0) & (base_stat <= 29)) {
-							color = "red";
-						} else if ((base_stat > 29) & (base_stat <= 59)) {
-							color = "orange";
-						} else if ((base_stat > 59) & (base_stat <= 89)) {
-							color = "yellow";
-						} else if ((base_stat > 89) & (base_stat <= 119)) {
-							color = "green";
-						} else if ((base_stat > 119) & (base_stat <= 149)) {
-							color = "dark_green";
-						} else if ((base_stat > 149) & (base_stat <= 255)) {
-							color = "blue";
-						}
+                        const getColor = (baseStat) => {
+                            if (baseStat <= 29) return "red";
+                            if (baseStat <= 59) return "orange";
+                            if (baseStat <= 89) return "yellow";
+                            if (baseStat <= 119) return "green";
+                            if (baseStat <= 149) return "dark_green";
+                            return "blue";
+                        };
 
-						return (
-							<div className="stat" key={key}>
-								<div className="name">{statInfo.name}</div>
-								<div>{base_stat}</div>
-								<div style={{ width: statPercentage + "%" }} className={`stat_bar bar_${color}`}></div>
-							</div>
-						);
-					})}
-				</div>
-				<div className="abilities">
-					<div>Abilities: </div>
-					<div className="abilities_container">
-						{abilities.map((ability, index) => {
-							return <span key={index}>{ability.ability.name}</span>;
-						})}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                        const color = getColor(base_stat);
+
+                        return (
+                            <div className="stat" key={key}>
+                                <div className="name">{statName}</div>
+                                <div>{base_stat}</div>
+                                <div style={{ width: `${statPercentage}%` }} className={`stat_bar bar_${color}`}></div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="abilities">
+                    <div>Abilities: </div>
+                    <div className="abilities_container">
+                        {abilities.map((ability, index) => {
+                            return <span key={index}>{ability.ability.name}</span>;
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PokemonCard;
